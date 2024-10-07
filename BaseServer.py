56b16587eb9bf0e70641c0c054ae0c76fdc5188b
Tutsam Singh <94393987+TutsamS@users.noreply.github.com>
2024-10-07 00:06:27 -0700

    Add files via upload

---
+++ BaseServer.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 14:27:53 2024
+
+@author: visingh
+"""
+
+import flightsbackend
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import urllib.parse
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    
+    def serve_image(self):
+        # Path to the image file in the same directory as BaseServer.py
+        image_path = os.path.join(os.path.dirname(__file__), "pm4.png")
+        
+        # Check if the image exists
+        if os.path.exists(image_path):
+            self.send_response(200)
+            self.send_header("Content-type", "image/png")
+            self.end_headers()
+            
+            # Read the image file in binary mode and write it to the response
+            with open(image_path, "rb") as file:
+                self.wfile.write(file.read())
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(bytes("Image not found", "utf-8"))
+            
+            
+    def get_Marker_Script(self, suf, lat, lng, label, origin_country, altitude, icao24, velocity, heading, depart_airport, arrive_airport):
+        infoContent = ""
+        if label:
+            infoContent = infoContent + "CallSign: <b>" + label + "</b><br>"
+        if origin_country:
+            infoContent = infoContent + "Country: <b>" + origin_country + "</b><br>"
+        if altitude:
+            infoContent = infoContent + "Altitude: <b>" + str(altitude) + " meters </b><br>"
+        if icao24:
+            infoContent = infoContent + "ICAO24: <b>" + str(icao24) + "</b><br>"
+        if velocity:
+            infoContent = infoContent + "Velocity: <b>" + str(velocity) + "</b><br>"
+        if heading:
+            infoContent = infoContent + "Heading: <b>" + str(heading) + "</b><br>"
+        if depart_airport:
+            infoContent = infoContent + "Departure: <b>" + str(depart_airport) + "</b><br>"
+        if arrive_airport:
+            infoContent = infoContent + "Arrival: <b>" + str(arrive_airport) + "</b><br>"
+        sc = 'var marker' + str(suf) + ' = new google.maps.Marker({position: {lat: ' + str(lat) + ', lng: ' + str(lng) + '},map: map, label: "' + label + '", icon: "/pm4.png"});'
+        sc = sc + "var infoWindow" + str(suf) + " = new google.maps.InfoWindow({content: '" + infoContent + "' });"
+        sc = sc + "marker"+ str(suf) + ".addListener('mouseover', function() {infoWindow"+ str(suf) + ".open(map, marker"+ str(suf) + ");setTimeout(function() {document.querySelector('.gm-ui-hover-effect').style.display = 'none';}, 10);});"
+        sc = sc + "marker"+ str(suf) + ".addListener('mouseout', function() {infoWindow"+ str(suf) + ".close();});"
+        return sc
+    
+    #label: { text: "' + label + '",fontWeight: "bold"}
+    #"setTimeout(function() {document.querySelector('.gm-ui-hover-effect').style.display = 'none';}, 10);"
+    #".gm-ui-hover-effect {display: none;}"
+    
+    def do_GET(self):
+        if self.path == "/pm4.png":
+            self.serve_image()
+        else:
+            city_name = urllib.parse.unquote(self.path)[1:]
+            try:
+                city_location = flightsbackend.get_city_location(city_name)
+                flights = flightsbackend.get_flights_over_city(city_location)
+                buttonClickScript = 'function redirectToLocation() { var location = document.getElementById("locationInput").value; if (location) {window.location.href = "/" + location;}}'
+                myscrpt = 'function initMap() {'
+                myscrpt = myscrpt + "var map = new google.maps.Map(document.getElementById('map'), {zoom: 10,center: {lat: " + str(city_location.latitude) + ", lng: " + str(city_location.longitude) + "}});"
+                
+                if flights:
+                    counter = 1
+                    for flight in flights:
+                        print(f"Flight {flight['callsign']} (ICAO24: {flight['icao24']}) from {flight['origin_country']} at [{flight['latitude']}, {flight['longitude']}] at altitude {flight['altitude']} meters.")
+                        myscrpt = myscrpt + self.get_Marker_Script(counter, flight['latitude'], 
+                                                                   flight['longitude'], 
+                                                                   flight['callsign'], 
+                                                                   flight['origin_country'], 
+                                                                   flight['altitude'], 
+                                                                   flight['icao24'],
+                                                                   flight['velocity'],
+                                                                   flight['heading'],
+                                                                   flight['departure_airport'],
+                                                                   flight['arrival_airport'])
+                        counter = counter + 1
+                myscrpt = myscrpt + '}'
+                
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(bytes("<html><head><title>The Flight Project</title>", "utf-8"))
+                self.wfile.write(bytes('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4ibafqYQTH9wA3OaoTeMWUB-tNo6GxFg"></script>', "utf-8"))
+                self.wfile.write(bytes('<script>' +myscrpt + buttonClickScript + '</script>', "utf-8"))
+                self.wfile.write(bytes("<style>html, body {height: 100%;margin: 0;padding: 0;overflow: hidden;} #map {height: 100%;width: 100%;position: fixed;top: 0;left: 0;z-index: 0;z-index: 0;}", "utf-8"))
+                self.wfile.write(bytes(".top-center-form {position: absolute;top: 20px;left: 50%;transform: translateX(-50%);background: rgba(255, 255, 255, 0.8);padding: 10px;border-radius: 8px;box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);z-index: 1;} .gm-ui-hover-effect {display: none;}</style>", "utf-8"))
+                self.wfile.write(bytes("</head>", "utf-8"))
+                self.wfile.write(bytes('<body onload="initMap()">', "utf-8"))
+                self.wfile.write(bytes('<div id="map"></div>', "utf-8"))
+                self.wfile.write(bytes('<div class="top-center-form"> <input type="text" id="locationInput" placeholder="Enter location" /><button onclick="redirectToLocation()">Go</button></div>', "utf-8"))
+                self.wfile.write(bytes("</body></html>", "utf-8"))
+            except Exception as e:
+                #myscript2 = 
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(bytes("<html><head><title>The Flight Project</title>", "utf-8"))
+                self.wfile.write(bytes("</head>", "utf-8"))
+                self.wfile.write(bytes("<body>", "utf-8"))
+                self.wfile.write(bytes("<p>No flights found over " + city_name + "</p>", "utf-8"))
+                self.wfile.write(bytes("</body></html>", "utf-8"))
+    
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+    
+    
+
---
+++ flightsbackend.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 22:31:36 2024
+
+@author: visingh
+"""
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+import requests
+from requests.auth import HTTPBasicAuth
+import webbrowser
+import time
+
+def get_city_location(city_name):
+    # Initialize geolocator
+    geolocator = Nominatim(user_agent="bounding_box_app")
+    
+    # Get the location details for the city
+    location = geolocator.geocode(str(city_name))
+    if location is None:
+        return f"City '{city_name}' not found."
+    return location
+
+def get_flight_details(icao24):
+    current_time = int(time.time())
+    begin_time = current_time - 3600 * 24  # Start time (24 hours ago)
+    
+    USERNAME = 'vikramsinghfremont'
+    PASSWORD = '51n9hOs!'
+    url = f"https://opensky-network.org/api/flights/aircraft?icao24={icao24}&begin={begin_time}&end={current_time}"
+    
+    try:
+        response = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
+        response.raise_for_status()
+        flights = response.json()
+        
+        if flights:
+            latest_flight = flights[-1]  # Get the most recent flight
+            return {
+                "callsign": latest_flight['callsign'],
+                "departure_airport": latest_flight['estDepartureAirport'],
+                "arrival_airport": latest_flight['estArrivalAirport'],
+                "departure_time": latest_flight['firstSeen'],
+                "arrival_time": latest_flight['lastSeen'],
+            }
+        
+        return "No recent flights found for this aircraft."
+    
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred: {e}"
+
+def get_bounding_box(city_location, distance_km=10):
+    
+    # City latitude and longitude
+    city_lat = city_location.latitude
+    city_lon = city_location.longitude
+    
+    # Calculate the four corners of the bounding box by moving the distance in each direction
+    # Move north (latitude increases)
+    north_point = geodesic(kilometers=distance_km).destination((city_lat, city_lon), 0)
+    # Move south (latitude decreases)
+    south_point = geodesic(kilometers=distance_km).destination((city_lat, city_lon), 180)
+    # Move east (longitude increases)
+    east_point = geodesic(kilometers=distance_km).destination((city_lat, city_lon), 90)
+    # Move west (longitude decreases)
+    west_point = geodesic(kilometers=distance_km).destination((city_lat, city_lon), 270)
+    
+    
+    # Define the bounding box
+    bounding_box = {
+        "latitude_min": south_point.latitude,
+        "latitude_max": north_point.latitude,
+        "longitude_min": west_point.longitude,
+        "longitude_max": east_point.longitude
+    }
+    
+    return bounding_box
+
+
+def get_flights_in_area(latitude_min, latitude_max, longitude_min, longitude_max):
+    USERNAME = 'vikramsinghfremont'
+    PASSWORD = '51n9hOs!'
+
+    url = "https://opensky-network.org/api/states/all"
+    
+    try:
+        response = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
+        
+        if response.status_code == 200:
+            data = response.json()
+            flights = data.get('states', [])
+            
+            # Filter flights over the specified area (bounding box)
+            flights_over_city = []
+            for flight in flights:
+                latitude = flight[6]
+                longitude = flight[5]
+                altitude = flight[7]
+                
+                if latitude and longitude: # and altitude:  # If altitude is 0 or NONE done display
+                    if latitude_min <= latitude <= latitude_max and longitude_min <= longitude <= longitude_max:
+                        icao24 = flight[0]
+                        departure_airport = None
+                        arrival_airport = None
+                        try:
+                            flight_details = get_flight_details(icao24)
+                            departure_airport = flight_details['departure_airport']
+                            arrival_airport = flight_details['arrival_airport']
+                        except Exception as e:
+                            print(f"Error fetching flight details for : {icao24}, {e}")
+                            
+                        flights_over_city.append({
+                            'icao24': icao24,
+                            'callsign': flight[1].strip(),
+                            'origin_country': flight[2],
+                            'longitude': flight[5],
+                            'latitude': flight[6],
+                            'altitude': flight[7],
+                            'velocity': flight[9],
+                            'heading': flight[10],
+                            'departure_airport': departure_airport,
+                            'arrival_airport': arrival_airport
+                        })
+            
+            return flights_over_city if flights_over_city else None #"No flights found over the specified area."
+        else:
+            return f"Error fetching data: {response.status_code}"
+    
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+def get_flights_over_city(city_location):
+    bb = get_bounding_box(city_location, distance_km=10)
+    latitude_min = bb['latitude_min']
+    latitude_max = bb['latitude_max']
+    longitude_min = bb['longitude_min']
+    longitude_max = bb['longitude_max']
+    flights = get_flights_in_area(latitude_min, latitude_max, longitude_min, longitude_max)
+    return flights
+    
+
+
+if __name__ == "__main__":        
+    # Define your city's bounding box (latitude and longitude)
+    # Example: Coordinates for New York City, USA
+    # latitude_min = 40.4774  # Minimum latitude
+    # latitude_max = 40.9176  # Maximum latitude
+    # longitude_min = -74.2591  # Minimum longitude
+    # longitude_max = -73.7004  # Maximum longitude
+    # bounding_box = get_bounding_box("New York, USA", distance_km=10)
+    # latitude_min = bounding_box['latitude_min']
+   #  latitude_max = bounding_box['latitude_max']
+    # longitude_min = bounding_box['longitude_min']
+   #  longitude_max = bounding_box['longitude_max']
+    
+    # Get flights over the specified city
+    flights = get_flights_over_city(get_city_location("Seattle, USA"))
+    
+    # Print the results
+    if isinstance(flights, list):
+        urlparams = "markers="
+        for flight in flights:
+            print(f"Flight {flight['callsign']} (ICAO24: {flight['icao24']}) from {flight['origin_country']} at [{flight['latitude']}, {flight['longitude']}] at altitude {flight['altitude']} meters.")
+            urlparams = urlparams + str(flight['latitude']) + "," + str(flight['longitude']) + "|"
+        
+        urlparams = urlparams + "&size=1280x1280&key=AIzaSyD4ibafqYQTH9wA3OaoTeMWUB-tNo6GxFg"     
+        print(urlparams)
+        webbrowser.open('https://maps.googleapis.com/maps/api/staticmap?' + urlparams) 
+    else:
+        print(flights)
+
